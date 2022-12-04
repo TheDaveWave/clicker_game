94d7a50f77af7848082cdc8800a62a1e80cd6db9
David Lindberg <david.wesley.lindberg@gmail.com>
2022-12-04 13:53:13 -0600

    added some basic components

---
+++ src/components/Home/HomePage.jsx
@@ -0,0 +1,10 @@
+
+function HomePage(){
+    return (
+        <div>
+            <h1>This is the home page.</h1>
+        </div>
+    );
+}
+
+export default HomePage;
---
+++ src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,10 @@
+
+function LoginPage(){
+    return (
+        <div>
+            <h1>This is the login page.</h1>
+        </div>
+    );
+}
+
+export default LoginPage;
