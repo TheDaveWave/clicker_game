d17fbcf6febf3fb1a60cd6798dc3bb807090a362
David Lindberg <david.wesley.lindberg@gmail.com>
2022-12-05 12:51:18 -0600

    upgrades work but the timer is a few seconds off

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -5,23 +5,26 @@ function HomePage() {
   const [clicks, setClicks] = useState(0);
   const [count, setCount] = useState(0);
   const [points, setPoints] = useState(160);
-  const [miliseconds, setMiliseconds] = useState(5000);
+  const [seconds, setSeconds] = useState(5);
   const [multiplier, setMultiplier] = useState(1);
   const [upgrade, setUpgrade] = useState(false);
   const [upgradeTwo, setUpgradeTwo] = useState(false);
   const [upgradeThree, setUpgradeThree] = useState(false);
 
-  let interval;
+  const [time, setTime] = useState(seconds);
+  const [timeVal, setTimeVal] = useState(0);
+
+  let timer;
 
   const handleClick = () => {
     setClicks(clicks + multiplier);
     setCount(count + multiplier);
-    console.log("clicks:", clicks);
+    // console.log("clicks:", clicks);
     // since react state is update on the next render
     // and a point needs to be added every 10 clicks.
     // we check if the next count is 10 and add a point.
     // does not work if count is an odd number.
-    if (clicks + multiplier === 10) {
+    if (clicks + multiplier >= 10) {
       setClicks(0);
       setPoints(points + 1);
     }
@@ -40,7 +43,7 @@ function HomePage() {
         break;
       case 3:
         setPoints(points - 100);
-        setMiliseconds(4000);
+        setSeconds(4);
         setUpgradeThree(true);
         break;
       default:
@@ -50,35 +53,57 @@ function HomePage() {
 
   const cancelUpgrade = (id) => {
     if (id === 1) {
+      console.log("canceled");
       setPoints(points + 10);
       setUpgrade(false);
     }
-    clearInterval(interval);
+    return clearInterval(timer);
+  };
+
+  const checkInterval = () => {
+    if (upgrade) {
+      // create timer where every n seconds,
+      // count is incremented by 1.
+      timer =
+        !timer &&
+        setInterval(() => {
+          setTimeVal(timeVal + 1);
+        //   console.log(timeVal);
+          if (timeVal === 1000) {
+            setTimeVal(0);
+            if (time === 1) {
+              handleClick();
+              setTime(seconds + 1);
+            }
+            setTime((prevTime) => prevTime - 1);
+          }
+        }, 1);
+      // setInterval creates an interval id and this useEffect constructs and
+      // then deconstructs the setInterval so it has a different Id everytime.
+      // This means that using a loop to clear out multiple intervals may not be possible.
+      // console.log(timer);
+    }
   };
 
   // the interval seems to reset on every click of the click button,
   // or when state is updated.
   useEffect(() => {
     // console.log("setting count", count);
-    if (upgrade) {
-      // create interval where every n seconds,
-      // count is incremented by 1.
-      interval = setInterval(() => handleClick(), miliseconds);
-      // setInterval creates an interval id and this useEffect constructs and
-      // then deconstructs the setInterval so it has a different Id everytime.
-      // This means that using a loop to clear out multiple intervals may not be possible.
-      // console.log(interval);
-    }
+    checkInterval();
     // cleanup function to clear the interval.
     // seems to have fixed bug where the setCount in interval was
     // grabbing the wrong instance of the count state.
-    return () => clearInterval(interval);
+    return () => {
+      clearInterval(timer);
+    };
   });
+  // does count and upgrade need to be in the dependencies?
 
   return (
     <div>
       <h1>This is the game</h1>
       <p>Points: {points}</p>
+      {upgrade && <p>Timer: {time}s</p>}
       <p>Count: {count}</p>
       <button onClick={() => handleClick()}>Click Me!</button>
       <div>
