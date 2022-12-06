b96465c797e4876c5669302f54ae82459bef0865
David Lindberg <david.wesley.lindberg@gmail.com>
2022-12-06 14:52:23 -0600

    added comments to the sagas

--- src/redux/sagas/count.saga.js
+++ src/redux/sagas/count.saga.js
@@ -9,32 +9,36 @@ function* countSaga() {
   yield takeEvery("ADD_CLICK", addClick);
 }
 
-// saga that will utilize setInterval to add a count
-// to the count reducer. The action payload will be
-// the multiplier.
+// saga that calls itself after its been dispatched to.
+// calls add_click every n seconds.
 function* autoClicker(action) {
   const multiplier = action.payload;
   yield delay(2000);
-  yield put({ type: "ADD_CLICK", payload: multiplier});
+  yield put({ type: "ADD_CLICK", payload: multiplier });
   yield put({ type: "AUTO_CLICKER", payload: action.payload });
 }
 
+// used to check how many clicks there are and
+// updated the store accordingly.
 function* checkClicks(action) {
+  // access the current state of the store by using the select effect.
   const state = yield select();
   const clicks = state.clickReducer;
   const multiplier = action.payload;
-  if ((clicks * multiplier) >= 10) {
-    console.log(true);
+  // check if clicks * multiplier are greater than or equal to ten
+  // if so reset clicks and add a point.
+  if (clicks * multiplier >= 10) {
     yield put({ type: "RESET_CLICKS" });
     yield put({ type: "INCREMENT_POINTS" });
   }
 }
 
+// now adds a click, count, and then dispatches checkClicks.
 function* addClick(action) {
-    const multiplier = action.payload;
-    yield put({ type: "INCREMENT_CLICKS", payload: multiplier });
-    yield put({ type: "INCREMENT_COUNT", payload: multiplier });
-    yield put({ type: "CHECK_CLICKS", payload: multiplier });
+  const multiplier = action.payload;
+  yield put({ type: "INCREMENT_CLICKS", payload: multiplier });
+  yield put({ type: "INCREMENT_COUNT", payload: multiplier });
+  yield put({ type: "CHECK_CLICKS", payload: multiplier });
 }
 
 export default countSaga;
